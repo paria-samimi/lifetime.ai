e6e48bc6fedb55d075782f0acafb121a8c5b5663
pariasamimi <dfg.ai.lifetime@gmail.com>
2025-03-04 18:39:36 +0100

    Intitial commit for Render deployment

---
+++ src/app/metadata-form/metadata-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-form',
+  imports: [],
+  templateUrl: './metadata-form.component.html',
+  styleUrl: './metadata-form.component.scss'
+})
+export class MetadataFormComponent {
+
+}
